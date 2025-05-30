0f4be8e4a2d3c317fb4b097375a925c43b479e65
sjhl <25_sjh0310@dshs.kr>
2025-05-30 15:47:39 +0900

    10818

--- 10818.py
+++ 10818.py
@@ -1,4 +1,4 @@
-# 1. 홀짝 판별기기
+# 1. 홀짝 판별기
 
 i = input("정수를 입력하시오 > ")
 k = int(i) % 2
@@ -11,8 +11,25 @@ else:
 
 i = int(input("숫자를 입력하세요: "))
 
-i = i + 1
+for n in range(1, i +1):
+    print("*" * n)
 
-for i in range(1, i):
-    print("*" * i)
+#3. 업 다운 게임 만들기
 
+import random
+
+answer = int(random.randint(1, 10))
+
+print(answer)
+for i in range(3):
+    n = int(input("숫자를 입력하세요 > "))
+
+    if n < answer:
+        print("UP!")
+    elif n > answer:
+        print("DOWN!")
+    else:
+        print("정답입니다.")
+        break
+
+print("정답은 " + str(answer) + "입니다.")
